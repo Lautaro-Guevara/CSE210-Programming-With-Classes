158c3cc8d06bc9398fd4302a8c3c8f5f464e9f70
Lautaro-Guevara <lfguevara2001@gmail.com>
2024-11-09 23:20:24 -0300

    W01 Prove - C# Prep 3 - Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+
+        int magicNumber = randomNumber.Next(1, 100);
+
+        int guess;
+
+
+        do
+        {
+            Console.WriteLine("What is your guess");
+            string response2 = Console.ReadLine();
+            guess = int.Parse(response2);
+        
+        
+            if (magicNumber == guess)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else if (magicNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("Lower");
+        };
+        } while (magicNumber != guess);
+        
     }
 }
